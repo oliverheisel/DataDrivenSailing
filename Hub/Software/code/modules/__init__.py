c8082d12af89e847ce27f23fc3bedb261752bb1a
oliverheisel <oliver@heisel.one>
2025-06-28 21:15:13 +0200

    Restructure to a single codebase

--- Hub/Software/code/modules/__init__.py
+++
@@ -1,6 +0,0 @@
-from modules import led
-from modules import gps_SAM_M10Q as gps
-from modules import imu_LSM6DSO as imu
-from modules import mag_mmc56x3 as mag
-from modules import battery_MAX17048 as battery
-from modules import wind_calypsomini as wind
